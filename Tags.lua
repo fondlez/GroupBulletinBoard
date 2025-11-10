478151a9a0f747e8b83010b528a9b8076ffcb5b7
fondlez <fondlez@protonmail.com>
2025-11-10 14:41:28 +0000

    Improve blacklist tags

--- Tags.lua
+++ Tags.lua
@@ -36,7 +36,7 @@ GBB.searchTagsLoc = langSplit( {
 } )
 
 GBB.badTagsLoc = langSplit( {
-  enGB = "2s 3s 5s 2v2 2vs2 3v3 3v3s 5v5 5vs5 boost recruit apply social twitch stream hardcore times team speaking speaker core cores",
+  enGB = "2s 3s 5s 2v2 2vs2 3v3 3v3s 5v5 5vs5 boost recruit apply social roster raids twitch stream hardcore times team speaking speaker core cores",
   deDE = "fc",
   ruRU = "гильдию гильдия слой",
   frFR = "",
